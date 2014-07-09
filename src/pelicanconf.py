97c0a9a7a3286b93b082bf055ac4f204f4f8b2c6
Jarkko Saltiola <jarkko.saltiola@gmail.com>
2014-07-09 18:57:44 +0300

    Show /pages/ links in navi.

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -17,7 +17,7 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-THEME="texris-proto"
+THEME="texris"
 
 # Blogroll
 LINKS =  (('Pelican', 'http://getpelican.com/'),
@@ -31,6 +31,8 @@ ARTICLE_SAVE_AS = "posts/{slug}/index.html"
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
+
+DISPLAY_PAGES_ON_MENU = True
 
 DEFAULT_PAGINATION = 5
 
